ca98567c2be6d6cca70a6ac7ea4ae2ce23d99831
HAOYUatHZ <haoyu@protonmail.com>
2020-11-30 16:01:04 +0800

    minor

--- src/io.rs
+++ src/io.rs
@@ -22,7 +22,7 @@ fn get_universal_setup_file_buff_reader(setup_file_name: &str) -> Result<BufRead
 }
 
 pub fn load_key_monomial_form<E: Engine>(filename: &str) -> Crs<E, CrsForMonomialForm> {
-    let mut buf_reader = get_universal_setup_file_buff_reader(filename).unwrap();
+    let mut buf_reader = get_universal_setup_file_buff_reader(filename).expect("read SRS file err");
     Crs::<E, CrsForMonomialForm>::read(&mut buf_reader).expect("read SRS err")
 }
 
@@ -30,7 +30,7 @@ pub fn maybe_load_key_lagrange_form<E: Engine>(maybe_filename: Option<String>) -
     match maybe_filename {
         None => None,
         Some(filename) => {
-            let mut buf_reader = get_universal_setup_file_buff_reader(&filename).unwrap();
+            let mut buf_reader = get_universal_setup_file_buff_reader(&filename).expect("read SRS file err");
             let key_lagrange_form = Crs::<E, CrsForLagrangeForm>::read(&mut buf_reader).expect("read SRS err");
             Some(key_lagrange_form)
         }
