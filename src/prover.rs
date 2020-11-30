48a97cc89560ac47b29a0561af223726d9430987
HAOYUatHZ <haoyu@protonmail.com>
2020-11-30 14:31:02 +0800

    some clean up

--- src/prover.rs
+++ src/prover.rs
@@ -24,8 +24,6 @@ use bellman_ce::{
 pub const SETUP_MIN_POW2: u32 = 20;
 pub const SETUP_MAX_POW2: u32 = 26;
 
-// pub struct PlonkVerificationKey<E: Engine>(VerificationKey<E, PlonkCsWidth4WithNextStepParams>);
-
 // fn base_universal_setup_dir() -> Result<PathBuf, anyhow::Error> {
 //     let mut dir = PathBuf::new();
 //     // root is used by default for provers
@@ -64,55 +62,6 @@ pub const SETUP_MAX_POW2: u32 = 26;
 //     let setup_file_name = format!("setup_2^{}_lagrange.key", power_of_two);
 //     let mut buf_reader = get_universal_setup_file_buff_reader(&setup_file_name)?;
 //     Ok(Crs::<E, CrsForLagrangeForm>::read(&mut buf_reader).map_err(|e| format_err!("Failed to read Crs from setup file: {}", e))?)
-// }
-
-// pub struct SetupForStepByStepProver<E: Engine> {
-//     setup_polynomials: SetupPolynomials<E, PlonkCsWidth4WithNextStepParams>,
-//     hints: Vec<(usize, TranspilationVariant)>,
-//     setup_power_of_two: u32,
-//     key_monomial_form: Option<Crs<E, CrsForMonomialForm>>,
-// }
-
-// impl<E: Engine> SetupForStepByStepProver<E> {
-//     pub fn prepare_setup_for_step_by_step_prover<C: Circuit<E> + Clone>(circuit: C) -> Result<Self, anyhow::Error> {
-//         let hints = transpile(circuit.clone())?;
-//         let setup_polynomials = setup(circuit, &hints)?;
-//         let size = setup_polynomials.n.next_power_of_two().trailing_zeros();
-//         let setup_power_of_two = std::cmp::max(size, SETUP_MIN_POW2); // for exit circuit
-//         let key_monomial_form = Some(get_universal_setup_monomial_form(setup_power_of_two)?);
-//         Ok(SetupForStepByStepProver {
-//             setup_power_of_two,
-//             setup_polynomials,
-//             hints,
-//             key_monomial_form,
-//         })
-//     }
-
-//     pub fn gen_step_by_step_proof_using_prepared_setup<C: Circuit<E> + Clone>(
-//         &self,
-//         circuit: C,
-//         vk: &PlonkVerificationKey<E>,
-//     ) -> Result<(), anyhow::Error> {
-//         let timer = Instant::now();
-//         let proof = prove_by_steps::<_, _, RollingKeccakTranscript<<E as ScalarEngine>::Fr>>(
-//             circuit,
-//             &self.hints,
-//             &self.setup_polynomials,
-//             None,
-//             self.key_monomial_form.as_ref().expect("Setup should have universal setup struct"),
-//         )?;
-//         log::info!("Proving takes {:?}", timer.elapsed());
-//         log::info!("Proof generated");
-
-//         let proof_path = "testdata/poseidon/proof.bin";
-//         let writer = File::create(proof_path).unwrap();
-//         proof.write(writer).unwrap();
-//         log::info!("Proof saved to {}", proof_path);
-
-//         let valid = verify::<_, RollingKeccakTranscript<<E as ScalarEngine>::Fr>>(&proof, &vk.0)?;
-//         anyhow::ensure!(valid, "proof for block is invalid");
-//         Ok(())
-//     }
 // }
 
 pub struct SetupForProver<E: Engine> {
