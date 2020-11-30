c82c3178bacf8f9b9209e1f61f0826bb0df82925
HAOYUatHZ <haoyu@protonmail.com>
2020-11-30 15:50:07 +0800

    clean up

--- src/prover.rs
+++ src/prover.rs
@@ -1,5 +1,4 @@
-// // Most of this file is modified from source codes of [Matter Labs](https://github.com/matter-labs)
-// use anyhow::format_err;
+// Most of this file is modified from source codes of [Matter Labs](https://github.com/matter-labs)
 use bellman_ce::pairing::Engine;
 use bellman_ce::{
     bn256::Bn256,
@@ -13,65 +12,9 @@ use bellman_ce::{
     },
     Circuit, ScalarEngine, SynthesisError,
 };
-// use std::io::BufReader;
-// use std::path::PathBuf;
-// use std::time::Instant;
-// use std::{fs::remove_file, fs::File, path::Path, thread};
-
-// use crate::circom_circuit::{r1cs_from_json_file, witness_from_json_file, CircomCircuit};
-// use crate::proofsys_type::ProofSystem;
 
 pub const SETUP_MIN_POW2: u32 = 20;
 pub const SETUP_MAX_POW2: u32 = 26;
-
-// fn check_power_of_two(power_of_two: &u32) -> std::result::Result<(), anyhow::Error> {
-//     anyhow::ensure!(
-//         (SETUP_MIN_POW2..=SETUP_MAX_POW2).contains(&power_of_two),
-//         "setup power of two is not in the correct range"
-//     );
-
-//     Ok(())
-// }
-
-// fn base_universal_setup_dir() -> Result<PathBuf, anyhow::Error> {
-//     let mut dir = PathBuf::new();
-//     // root is used by default for provers
-//     dir.push("keys");
-//     dir.push("setup");
-//     anyhow::ensure!(dir.exists(), "Universal setup dir does not exits");
-//     Ok(dir)
-// }
-
-// fn get_universal_setup_file_buff_reader(setup_file_name: &str) -> Result<BufReader<File>, anyhow::Error> {
-//     let setup_file = {
-//         let mut path = base_universal_setup_dir()?;
-//         path.push(&setup_file_name);
-//         File::open(path).map_err(|e| format_err!("Failed to open universal setup file {}, err: {}", setup_file_name, e))?
-//     };
-//     Ok(BufReader::with_capacity(1 << 29, setup_file))
-// }
-
-// /// Returns universal setup in the monomial form of the given power of two (range: SETUP_MIN_POW2..=SETUP_MAX_POW2). Checks if file exists
-// pub fn get_universal_setup_monomial_form<E: Engine>(power_of_two: u32) -> Result<Crs<E, CrsForMonomialForm>, anyhow::Error> {
-//     anyhow::ensure!(
-//         (SETUP_MIN_POW2..=SETUP_MAX_POW2).contains(&power_of_two),
-//         "setup power of two is not in the correct range"
-//     );
-//     let setup_file_name = format!("setup_2^{}.key", power_of_two);
-//     let mut buf_reader = get_universal_setup_file_buff_reader(&setup_file_name)?;
-//     Ok(Crs::<E, CrsForMonomialForm>::read(&mut buf_reader).map_err(|e| format_err!("Failed to read Crs from setup file: {}", e))?)
-// }
-
-// /// Returns universal setup in lagrange form of the given power of two (range: SETUP_MIN_POW2..=SETUP_MAX_POW2). Checks if file exists
-// pub fn get_universal_setup_lagrange_form<E: Engine>(power_of_two: u32) -> Result<Crs<E, CrsForLagrangeForm>, anyhow::Error> {
-//     anyhow::ensure!(
-//         (SETUP_MIN_POW2..=SETUP_MAX_POW2).contains(&power_of_two),
-//         "setup power of two is not in the correct range"
-//     );
-//     let setup_file_name = format!("setup_2^{}_lagrange.key", power_of_two);
-//     let mut buf_reader = get_universal_setup_file_buff_reader(&setup_file_name)?;
-//     Ok(Crs::<E, CrsForLagrangeForm>::read(&mut buf_reader).map_err(|e| format_err!("Failed to read Crs from setup file: {}", e))?)
-// }
 
 pub struct SetupForProver<E: Engine> {
     setup_polynomials: SetupPolynomials<E, PlonkCsWidth4WithNextStepParams>,
