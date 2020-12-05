5d9cef72743dc5cc5cf3f1d1a05ea2f71e4fe569
HAOYUatHZ <haoyu@protonmail.com>
2020-12-05 08:50:41 +0800

    rename maybe_filename to option_filename

--- src/reader.rs
+++ src/reader.rs
@@ -44,8 +44,8 @@ pub fn load_key_monomial_form<E: Engine>(filename: &str) -> Crs<E, CrsForMonomia
     Crs::<E, CrsForMonomialForm>::read(&mut buf_reader).expect("read key_monomial_form err")
 }
 
-pub fn maybe_load_key_lagrange_form<E: Engine>(maybe_filename: Option<String>) -> Option<Crs<E, CrsForLagrangeForm>> {
-    match maybe_filename {
+pub fn maybe_load_key_lagrange_form<E: Engine>(option_filename: Option<String>) -> Option<Crs<E, CrsForLagrangeForm>> {
+    match option_filename {
         None => None,
         Some(filename) => {
             let mut buf_reader = get_universal_setup_file_buff_reader(&filename).expect("read key_lagrange_form file err");
